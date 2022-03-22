268e202b7b0e796a6ac59de42464dde3f1395659
Dustin Black <dblack@redhat.com>
2022-03-22 17:21:09 +0100

    add option to remove nudges from report/dash

--- nudge.py
+++ nudge.py
@@ -41,6 +41,11 @@ parser.add_argument(
     default=False,
     action='store_true',
     dest="activeOnly")
+parser.add_argument(
+    "--no_nudge",
+    default=False,
+    action='store_true',
+    dest="noNudge")
 args = parser.parse_args()
 
 dashFile = '/var/tmp/nudge.html'
@@ -177,19 +182,20 @@ if args.report or args.dash :
         #    print("<tr><td>")
 
         print("+{}+".format("="*100))
-        if len(epic) == 0 :
-            if args.report: print("\x1b[0;30;43m")
-            if args.dash: print('<div class="noepic">', end='')
-            print("\n -- NOTE:: {} has no EPIC assigned, please link to an EPIC -- ".format(nudge['JIRA']))
-            if args.dash: print('\n</div>', end='')
-            if args.report: print("\x1b[0m")
-        updated = datetime.strptime(nudge['UPDATED'].split('T')[0], '%Y-%m-%d')
-        if (datetime.now() - updated) > timedelta(days = 5) and 'Closed' not in nudge['STATUS'] and 'To Do' not in nudge['STATUS'] :
-            if args.report: print("\x1b[1;37;41m")
-            if args.dash: print('<div class="needsupdate">', end='')
-            print("\n -- NOTE:: Has not been updated since {}. Please {} provide an update. -- ".format(nudge['UPDATED'].split('T')[0], nudge['OWNER']))
-            if args.dash: print('\n</div>', end='')
-            if args.report: print("\x1b[0m")
+        if not args.noNudge :
+            if len(epic) == 0 :
+                if args.report: print("\x1b[0;30;43m")
+                if args.dash: print('<div class="noepic">', end='')
+                print("\n -- NOTE:: {} has no EPIC assigned, please link to an EPIC -- ".format(nudge['JIRA']))
+                if args.dash: print('\n</div>', end='')
+                if args.report: print("\x1b[0m")
+            updated = datetime.strptime(nudge['UPDATED'].split('T')[0], '%Y-%m-%d')
+            if (datetime.now() - updated) > timedelta(days = 5) and 'Closed' not in nudge['STATUS'] and 'To Do' not in nudge['STATUS'] :
+                if args.report: print("\x1b[1;37;41m")
+                if args.dash: print('<div class="needsupdate">', end='')
+                print("\n -- NOTE:: Has not been updated since {}. Please {} provide an update. -- ".format(nudge['UPDATED'].split('T')[0], nudge['OWNER']))
+                if args.dash: print('\n</div>', end='')
+                if args.report: print("\x1b[0m")
         print("\n{} - {} \nLabels: {}\nOwner: {}\nCreator: {}\nStatus: {}\nLink: {}\nUpdated: {}\nLast Comment:\n{}\n\n".
               format(nudge['JIRA'],
                      nudge['SUMM'],
